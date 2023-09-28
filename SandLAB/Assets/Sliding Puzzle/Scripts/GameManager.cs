0772b6924ea988afb8957c9345b18015c9416bd0
ISAQUE RAMOS FIGUEIREDO PETRI <irfpetri@gmail.com>
2023-09-28 10:40:02 -0300

    Arrumado Slide Puzzle
    De 4x4 foi para um 3x3

--- SandLAB/Assets/Sliding Puzzle/Scripts/GameManager.cs
+++ SandLAB/Assets/Sliding Puzzle/Scripts/GameManager.cs
@@ -58,7 +58,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         pieces = new List<Transform>();
-        size = 4;
+        size = 3;
         CreateGamePieces(0.01f);
     }
 
