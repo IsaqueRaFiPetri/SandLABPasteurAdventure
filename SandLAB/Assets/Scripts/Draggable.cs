d8572466883adda4680969087572ee6c1f34cad5
ISAQUE RAMOS FIGUEIREDO PETRI <irfpetri@gmail.com>
2023-10-30 17:00:13 -0300

    Continmuação do Puzzle que o professor pediu

---
+++ SandLAB/Assets/Scripts/Draggable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    public bool IsDragging;
+    public Collider2D _collider;
+    //private DragController _dragController;
+}
