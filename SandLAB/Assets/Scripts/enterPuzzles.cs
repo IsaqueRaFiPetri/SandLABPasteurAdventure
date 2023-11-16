9d5813b06fab739cfc0df932038f56eebb7d1cbe
ISAQUE RAMOS FIGUEIREDO PETRI <irfpetri@gmail.com>
2023-11-16 17:01:22 -0300

    Transferência
    Transferindo o puzzle das perguntas para um painel com as perguntas

--- SandLAB/Assets/Scripts/enterPuzzles.cs
+++ SandLAB/Assets/Scripts/enterPuzzles.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class enterPuzzles : MonoBehaviour
 {
-    public string sceneNameToLoad; // The name of the scene you want to load
+    public GameObject painel; // The name of the scene you want to load
     public KeyCode keyToPress = KeyCode.E; // Change this to the key you want to use
 
     public void OnTriggerStay2D()
     {
         if (Input.GetKey(KeyCode.E))
         {
-            SceneManager.LoadScene(sceneNameToLoad);
+            painel.SetActive(true);
         }
     }
 }
