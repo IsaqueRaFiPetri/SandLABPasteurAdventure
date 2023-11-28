f2bc21b1ebfa78fde4127a9077f4a1d08fe3f4dc
IsaqueRaFiPetri <irfpetri@gmail.com>
2023-11-28 18:44:43 -0300

    Tentativas e Erros
    Estou pensando em não fazermos o puzzle da experiencia.
    
    OBS: Medeiros, te mandei melhores explicações por mensagem.

--- SandLAB/Assets/Scripts/HeatBAr.cs
+++ SandLAB/Assets/Scripts/HeatBAr.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeatBAr : MonoBehaviour
+public class HeatBar : MonoBehaviour
 {
-    public GameObject heatPoint;
+    private Vector3 direction;
+    public float gravity = -1f;
+    public float strenght = 5f;
 
-
-    private void OnMouseDown()
+    private void Update()
     {
-        heatBar.transform.position += ;
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strenght;
+        }
+        /*if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase)
+        }*/
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
     }
 }
